3eca6e53f1aa458944bd0de85113104c82bc0688
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2009-08-21 15:46:27 -0700

    Fixed package issues

--- src/main/scala/com/biosimilarity/games/comet/AskName.scala
+++ src/main/scala/com/biosimilarity/games/comet/AskName.scala
@@ -13,7 +13,8 @@
  * See the License for the specific language governing permissions
  * and limitations under the License.
  */
-package net.liftweb.example.comet
+//package net.liftweb.example.comet
+package com.biosimilarity.games.comet
 
 import _root_.net.liftweb.http._
 import S._
